e7651fd4ad8e93d08467635d3f11ab639f96e217
Dustin L <lawson.dustin12@gmail.com>
2025-01-05 21:20:53 -0600

    todos2

---
+++ pages/todo/new.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ToDoForm from '../../src/components/ToDoForm';
+
+// TODO: create a reusable form to add/edit Author and render in this view
+
+export default function AddToDo() {
+  return <ToDoForm />;
+}
