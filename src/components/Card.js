1003c03721a5d4d8eaa6a45210b5f5eef5aeea8d
Jordan Youssef <jordanyoussef28@gmail.com>
2025-01-07 17:01:58 -0600

    working through errors, form creates

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
+'use client';
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Link from 'next/link';
-import { deleteList } from '../api/listData';
+// import Link from 'next/link';
+import { deleteList } from '@/api/listData';
 
 function ListCard({ listObj, onUpdate }) {
   const deleteThisList = () => {
@@ -13,20 +15,11 @@ function ListCard({ listObj, onUpdate }) {
   };
 
   return (
-    <Card
-      style={{
-        color: 'white',
-        width: '18rem',
-        margin: '10px',
-      }}
-    >
+    <Card style={{ width: '18rem', margin: '10px' }}>
       <Card.Body>
         <Card.Title>{listObj.task}</Card.Title>
 
-        <Link href={`/list/edit/${listObj.firebaseKey}`} passHref>
-          <Button variant="light">EDIT</Button>
-        </Link>
-        <Button variant="light" onClick={deleteThisList} className="m-2">
+        <Button variant="danger" onClick={deleteThisList} className="m-2">
           DELETE
         </Button>
       </Card.Body>
--- src/components/forms/ListForm.js
+++ src/components/forms/ListForm.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/compat/router';
+'use client';
+
+import React, { useState } from 'react';
+// import { useRouter } from 'next/navigation';
 import PropTypes from 'prop-types';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
-import { updateList, createList } from '../../api/listData';
+import { useAuth } from '../../utils/context/authContext';
+import { createList, updateList } from '../../api/listData';
 
 const initialState = {
   task: '',
-  firebaseKey: '',
 };
 
-function ListForm({ obj }) {
+function ListForm({ obj, onUpdate }) {
   const [formInput, setFormInput] = useState(initialState);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (obj.firebaseKey) {
-      setFormInput(obj);
-    } else {
-      setFormInput(initialState);
-    }
-  }, [obj]);
+  const { user } = useAuth();
+  // sets progress state to an empty array
+  // const router = useRouter();
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    // function designed to update form input state whenever a form value changes.
+    const { name, value } = e.target; // destructures name and value of input feild from event object
     setFormInput((prevState) => ({
+      // updates form input state and updates specific field with new value
       ...prevState,
       [name]: value,
     }));
@@ -34,31 +31,31 @@ function ListForm({ obj }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (obj.firebaseKey) {
-      const updatedList = {
-        id: obj.firebaseKey,
-        task: formInput.task,
-      };
-      updateList(updatedList).then(() => router.push('/'));
+      updateList(formInput).then(() => onUpdate());
     } else {
-      const payload = formInput.label;
-      createList(payload).then(() => {
-        setFormInput(initialState);
-      });
+      const payload = { ...formInput, uid: user.uid };
+      createList(payload)
+        .then((response) => {
+          // Get the Firebase key from the response
+          const firebaseKey = response.name;
+          // Update the list with the Firebase key
+          return updateList({ ...payload, firebaseKey });
+        })
+        .then(() => {
+          //         onUpdate();
+          setFormInput(initialState); // Reset form
+        });
     }
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Task</h2>
-
-      {/* Task INPUT  */}
-      <FloatingLabel controlId="floatingInput3" label="Task" className="mb-3">
-        <Form.Control type="text" placeholder="Enter New Task" name="task" value={formInput.task} onChange={handleChange} required />
-      </FloatingLabel>
-
-      {/* SUBMIT BUTTON  */}
+      <Form.Group controlId="floatingInput2" className="mb-3">
+        <Form.Label> Things To Do:</Form.Label>
+        <Form.Control className="height50" type="text" placeholder="Enter New Task" name="task" value={formInput.task} onChange={handleChange} required />
+      </Form.Group>
       <Button type="submit" className="copy-btn">
-        {obj.firebaseKey ? 'Update' : 'Add'} Task{' '}
+        {obj.firebaseKey ? 'Update' : 'Create'} List
       </Button>
     </Form>
   );
@@ -66,12 +63,15 @@ function ListForm({ obj }) {
 
 ListForm.propTypes = {
   obj: PropTypes.shape({
-    firebaseKey: PropTypes.string,
-    task: PropTypes.string,
+    // defines the structure of the prop
+    task: PropTypes.string.isRequired,
+    firebaseKey: PropTypes.string.isRequired,
   }),
+  onUpdate: PropTypes.func.isRequired,
 };
 
 ListForm.defaultProps = {
+  // provides default values for properties
   obj: initialState,
 };
 
